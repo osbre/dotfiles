878d8ba1cbc36488072d3ea42c2f4e9e7f188c96
Ostap Brehin <osbre@protonmail.com>
2023-10-15 17:07:15 +0100

    sync

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+fish_add_path ".config/composer/vendor/bin"
+
+
--- sync.sh
+++ sync.sh
@@ -1,2 +1,3 @@
 cp ~/.wezterm.lua ./.wezterm.lua
 cp -r ~/.config/nvim ./.config/nvim
+cp ~/.config/fish/config.fish ./.config/fish/config.fish
