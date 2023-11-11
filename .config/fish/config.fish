380c129b7d6505f72f5fd20e1c55d0e593ead015
Ostap Brehin <osbre@protonmail.com>
2023-11-11 01:09:21 +0000

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-fish_add_path ".config/composer/vendor/bin"
-
-
+fish_add_path ~/.composer/vendor/bin
+fish_add_path ~/.config/toolbox/bin
+fish_add_path ~/.cache/rebar3/bin
